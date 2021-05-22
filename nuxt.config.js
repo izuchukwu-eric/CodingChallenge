ecf3cc1f5860e60e3c1d3e48248766ee55196eb2
Izuchukwu Onukwube <onukwubeizu@gmail.com>
2021-05-23 00:14:51 +0100

    final commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  target: 'static',
+
   head: {
     title: 'CodingChallenge',
     htmlAttrs: {
@@ -36,12 +38,6 @@ export default {
   modules: ['@nuxtjs/axios'],
 
   axios: {},
-
-  target: 'static',
-
-  router: {
-    base: '/CodingChallenge/',
-  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
